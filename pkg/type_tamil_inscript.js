138710da2b14ce78b61386f4c26feacfcbf558ee
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2020-05-27 09:24:05 +0200

    Add ja

--- pkg/type_tamil_inscript.js
+++ pkg/type_tamil_inscript.js
@@ -203,7 +203,7 @@ function addBorrowedObject(obj) {
 }
 function __wbg_adapter_18(arg0, arg1, arg2) {
     try {
-        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1ce4263297bcdc58(arg0, arg1, addBorrowedObject(arg2));
+        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8aee580eb2bba620(arg0, arg1, addBorrowedObject(arg2));
     } finally {
         heap[stack_pointer++] = undefined;
     }
--- pkg/type_tamil_inscript_bg.d.ts
+++ pkg/type_tamil_inscript_bg.d.ts
@@ -6,7 +6,7 @@ export function start(): void;
 export function __wbindgen_malloc(a: number): number;
 export function __wbindgen_realloc(a: number, b: number, c: number): number;
 export const __wbindgen_export_2: WebAssembly.Table;
-export function _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1ce4263297bcdc58(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8aee580eb2bba620(a: number, b: number, c: number): void;
 export function __wbindgen_free(a: number, b: number): void;
 export function __wbindgen_exn_store(a: number): void;
 export function __wbindgen_start(): void;
