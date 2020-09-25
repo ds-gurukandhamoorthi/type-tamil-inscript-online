150f84477c8a9c7b060f9720c9cef12ce74c42b3
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2020-09-25 17:01:52 +0200

    Replace Naive Bayes with Logistic Regression

--- pkg/type_tamil_inscript.js
+++ pkg/type_tamil_inscript.js
@@ -402,7 +402,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper55 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper59 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 15, __wbg_adapter_18);
         return addHeapObject(ret);
     };
